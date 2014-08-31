642035becf041da2440a1cee613b3e4ed726ce02
David Christian <david.w.christian@gmail.com>
2014-08-31 15:30:58 -0700

    First Twilio phone Dial

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -6,8 +6,12 @@ skip_before_action :verify_authenticity_token
 	  
   def voice
     response = Twilio::TwiML::Response.new do |r|
-      r.Say 'Hey there. Congrats on integrating Twilio into your Rails 4 app.', :voice => 'alice'
-	  r.Play 'http://linode.rabasa.com/cantina.mp3'
+      #r.Say 'Hey there. Congrats on integrating Twilio into your Rails 4 app.', :voice => 'alice'
+	  #r.Play 'http://linode.rabasa.com/cantina.mp3'
+	  r.Say 'Hey There Andrew, I am calling you now. ', :voice => 'alice'
+  	   r.Dial :callerId => '+14404274157' do |d|
+    	 d.Client 'Buzz Me In'
+  		end
 	end
    
    render_twiml response
