08b6af254e6426da1607bad2f53d41944434fdbf
David Christian <david.w.christian@gmail.com>
2014-08-31 15:59:30 -0700

    edit2

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,7 +1,4 @@
-require 'twilio-ruby'
-# put your own credentials here
-account_sid = 'ACa15e235e82e5417d9d102c1992d4ac73'
-auth_token = '87954282ec5f13818d9c019dbb614fe1'
-
-# set up a client to talk to the Twilio REST API
-@client = Twilio::REST::Client.new account_sid, auth_token
+Twilio::Config.setup do
+  account_sid 'ACa15e235e82e5417d9d102c1992d4ac73'
+  auth_token  '87954282ec5f13818d9c019dbb614fe1'
+end
