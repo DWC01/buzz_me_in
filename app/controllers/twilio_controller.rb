6dfae9aaa351a4e932a0d20ebf90daa84008a4a3
David Christian <david.w.christian@gmail.com>
2014-08-31 16:58:26 -0700

    Added first attempt SMS- attempt 2

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -9,9 +9,9 @@ skip_before_action :verify_authenticity_token
 	  r.Say 'Hey There Andrew, I am calling you now. ', :voice => 'alice'
   	     r.Dial :callerId => '+19256399635' do |d|  	
     	  d.Number (CGI::escapeHTML '+14404274157') 
-    	  r.Say 'Hey There Andrew, Whats Up dog!?. ', :voice => 'alice'
-    	  r.Sms  "Here is the code to get in! Shhhh! -> 896"
+    	  d.Say 'Hey There Andrew, Whats Up dog!?. ', :voice => 'alice'
   		 end
+  		 r.Sms  "Here is the code to get in! Shhhh! -> 896"
 	end
    
    render_twiml twiml
