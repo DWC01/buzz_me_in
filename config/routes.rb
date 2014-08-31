9e6037fbd91b06f3dd39850425d5b46ab02f49fd
David Christian <david.w.christian@gmail.com>
2014-08-31 16:35:30 -0700

    Changed caller id num to 9256399635

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users, :buzz_me_apps 
   
   post 'twilio/voice' => 'twilio#voice'
+  post 'twilio/voice' => 'twilio#voice'
 
   root to: 'visitors#index'
 end
