072d2e6d743a6a275e7649d2405c25fb84b3681b
David Christian <david.w.christian@gmail.com>
2014-08-31 18:24:35 -0700

    edit 7

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -16,15 +16,15 @@ skip_before_action :verify_authenticity_token
 	  
   def voice
     twiml = Twilio::TwiML::Response.new do |r|
-	  r.Say 'Hey There Andrew, I am calling you now. ', :voice => 'alice'
-  	     r.Dial :callerId => CALLER_ID do |d|  	
-    	  d.Number (CGI::escapeHTML '+14404274157') 
-  		 end
+	  # r.Say 'Hey There Andrew, I am calling you now. ', :voice => 'alice'
+  	#      r.Dial :callerId => CALLER_ID do |d|  	
+   #  	  d.Number (CGI::escapeHTML '+14404274157') 
+  	# 	 end
   	# 	 r.Sms :callerId => CALLER_ID do |d| 
   	# 	 	"Here is the code to get in! Shhhh! -> 896"
-	     # r.Gather :action => 'first_user_response' do |g|
-	     #  g.Say 'Hey There Andrew! Presss One to enter a password to get Buzzed in', :voice => 'alice'
-	     # end
+	     r.Gather :action => 'first_user_response' do |g|
+	       g.Say 'Hey There Andrew! Presss One to enter a password to get Buzzed in', :voice => 'alice'
+	      end
 
 	end
    
@@ -32,12 +32,12 @@ skip_before_action :verify_authenticity_token
 
   end
 
-  # def first_user_response
-  # 	twiml = Twilio::TwiML::Response.new do |r|
-	 #  r.Say "This is Saying from the first user response method."
-  #    end
-  #   render_twiml twiml
-  # end 	
+  def first_user_response
+  	twiml = Twilio::TwiML::Response.new do |r|
+	  r.Say "This is Saying from the first user response method."
+     end
+    render_twiml twiml
+  end 	
 
   # def message
 
