61aee2d085a6d683eea4b923f618f5e0456e380d
David Christian <david.w.christian@gmail.com>
2014-08-31 18:33:13 -0700

    Added digits as collection 1

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -34,12 +34,12 @@ skip_before_action :verify_authenticity_token
 
   def first_user_response
   	twiml = Twilio::TwiML::Response.new do |r|
-  	  if Digits > 5	
+  	  if r.Digits > 5	
 	    r.Say "Your Digit is Greater than 5."
-	    r.Say Digits
+	    #r.Say Digits
 	  else
 	  	r.Say "Your Digit is Less than 5."
-	  	r.Say Digits
+	  	#r.Say Digits
      end
     render_twiml twiml
   end 	
