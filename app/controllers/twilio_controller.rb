8381fc7235c463e0eb54fbd3a6aafb9a78833553
David Christian <david.w.christian@gmail.com>
2014-08-31 17:24:05 -0700

    added message action

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -3,15 +3,26 @@ class TwilioController < ApplicationController
 include Webhookable
 after_filter :set_header
 skip_before_action :verify_authenticity_token
+
+ # your Twilio authentication credentials
+  ACCOUNT_SID = 'ACa15e235e82e5417d9d102c1992d4ac73'
+  ACCOUNT_TOKEN = '87954282ec5f13818d9c019dbb614fe1'
+
+  # base URL of this application
+  BASE_URL = "http://buzzmein.herokuapp.com/twilio"
+
+  # Outgoing Caller ID you have previously validated with Twilio
+  CALLER_ID = '+19256399635'
 	  
   def voice
     twiml = Twilio::TwiML::Response.new do |r|
 	  r.Say 'Hey There Andrew, I am calling you now. ', :voice => 'alice'
-  	     r.Dial :callerId => '+19256399635' do |d|  	
+  	     r.Dial :callerId => CALLER_ID do |d|  	
     	  d.Number (CGI::escapeHTML '+14404274157') 
     	  d.Say 'Hey There Andrew, Whats Up dog!?. ', :voice => 'alice'
   		 end
-  		 r.Sms  "Here is the code to get in! Shhhh! -> 896"
+  		 r.Sms :callerId => CALLER_ID do |d| 
+  		 	"Here is the code to get in! Shhhh! -> 896"
 	end
    
    render_twiml twiml
@@ -19,8 +30,16 @@ skip_before_action :verify_authenticity_token
   end
 
   def message
-    
 
+    @twilio_client = Twilio::REST::Client.new ACCOUNT_SID, ACCOUNT_TOKEN
+ 
+    @twilio_client.account.sms.messages.create(
+      :from => CALLER_ID,
+      :to => '+14404274157',
+      :body => "This is an message!!!"
+    )
+    end
+    
   end
  
 	
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :users, :buzz_me_apps 
   
-  post 'twilio/voice' => 'twilio#voice'
-  post 'twilio/voice' => 'twilio#voice'
+  post 'twilio/voice'    => 'twilio#voice'
+  post 'twilio/message'  => 'twilio#message'
 
   root to: 'visitors#index'
 end
